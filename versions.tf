5fe5032dcbb05de3dcdd29233f28e932797e0339
gogeekness <rdeseke@gmail.com>
2025-07-16 16:11:15 +0200

    debugging version adn teh providers

--- versions.tf
+++ versions.tf
@@ -8,12 +8,3 @@ terraform {
     }
   }
 }
-
-# terraform {
-#   required_providers {
-#     openstack = {
-#       source  = "terraform-provider-openstack/openstack"
-#       version = "~> 1.54.0"
-#     }
-#   }
-# }
