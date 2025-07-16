1383bc9842e21d4e4104dd1f98b4a45c7764cebf
gogeekness <rdeseke@gmail.com>
2025-07-16 16:25:07 +0200

    adding provider.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,7 @@
 
 
 # Configure the OpenStack Provider for Telekom Cloud
-provider "opentelekomcloud" {
-  auth_url    = var.auth_url
-  domain_name = var.domain_name
-  tenant_name = var.tenant_name
-  user_name   = var.user_name
-  password    = var.password != null ? var.password : lookup(var.env_vars, "OTC_PASSWORD", "")
-  region      = var.region
-  access_key  = var.user_name
-  secret_key  = var.password
-}
+
 
 
 
--- versions.tf
+++ versions.tf
@@ -3,6 +3,9 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     opentelekomcloud = {
+
+
+
       source  = "opentelekomcloud/opentelekomcloud"
       version = "~> 1.36"
     }
