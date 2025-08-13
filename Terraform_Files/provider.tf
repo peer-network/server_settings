c5a4c81b3a415bdb7e3daf16e86d4433042d330d
goggeekness <goggeekness@swcp.com>
2025-08-13 09:31:29 +0200

    Need env var filled to work.

--- Terraform_Files/provider.tf
+++ Terraform_Files/provider.tf
@@ -14,9 +14,10 @@ provider "opentelekomcloud" {
     auth_url    = var.auth_url
     domain_name = var.domain_name
     tenant_name = var.tenant_name
-    user_domain_name = var.user_domain_name
+    # user_domain_name = var.user_domain_name
     user_name   = var.user_name
-    password    = var.password #!= null ? var.password : lookup(var.env_vars, "OTC_PASSWORD", "")
+    password    = 
+                  #var.password #!= null ? var.password : lookup(var.env_vars, "OTC_PASSWORD", "")
     region      = var.region
     # access_key  = var.user_name
     # secret_key  = var.password
