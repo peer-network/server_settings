9babe82c85b71f2c76326ad6f629071b73f52655
goggeekness <goggeekness@swcp.com>
2025-08-19 11:43:22 +0200

    Minor change for the output file.

--- Terraform_Files/export.tf
+++ Terraform_Files/export.tf
@@ -1,6 +1,6 @@
 # export.tf
 
-resource "local_sensitive_file" "peer_network_yaml" {
+resource "local_sensitive_file" "peer_network.yaml" {
   filename        = "peer_network.yaml"
   content         = yamlencode(local.peer_network)
   file_permission = "0640"
